48716f9a339e9944f2a2234099f43f9d6524248f
Liping Mechling <65612959+Lime5005@users.noreply.github.com>
2022-02-08 19:04:51 +0100

    Add edit and delete button to all records

--- src/components/RecordsList.vue
+++ src/components/RecordsList.vue
@@ -10,6 +10,8 @@
             <th>Note</th>
             <th>Created At</th>
             <th>Updated At</th>
+            <th>EDIT</th>
+            <th>DELETE</th>
           </tr>
         </thead>
         <tbody>
@@ -22,6 +24,20 @@
             <td>{{ record.note}}</td>
             <td>{{ record.createdAt | formatDateTime }}</td>
             <td>{{ record.updatedAt | formatDateTime }}</td>
+            <td>
+              <a class="btn btn-warning"
+                :href="'/records/' + record.id"
+              >
+                <fa-icon :icon="['fas', 'edit']"/>
+              </a>
+            </td>
+            <td>
+              <button class="btn btn-danger"
+                @click="deleteRecord(record.id)"
+              >
+                <fa-icon :icon="['fas', 'trash']"/>
+              </button>
+            </td>
            </tr>    
         </tbody>
       </table> 
@@ -48,6 +64,17 @@ export default {
         .then((response) => {
           this.records = response.data;
           console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+   
+    deleteRecord(id) {
+      RecordDataService.deleteRecord(id)
+        .then((response) => {
+          console.log(response.data);
+          this.getPatientRecords(this.$route.params.id);
         })
         .catch((error) => {
           console.log(error);
